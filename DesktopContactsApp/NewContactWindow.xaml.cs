fc4922fe1352a754722a3aa5f0b57315cd354524
Mike Liao <mike.hm.liao@gmail.com>
2020-01-18 12:43:52 +0800

    established sql connction

--- DesktopContactsApp/NewContactWindow.xaml.cs
+++ DesktopContactsApp/NewContactWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +28,23 @@ namespace DesktopContactsApp
 
         private void saveButton_Click(object sender, RoutedEventArgs e)
         {
-            // Save contact and close the window
+            //TODO: Save contact and close the window
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneTextBox.Text
+            };
+
+            string databaseName = "Contact.db";
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string databasePath = System.IO.Path.Combine(folderPath, databaseName);
+
+            SQLiteConnection connection = new SQLiteConnection(databasePath);
+            connection.CreateTable<Contact>(); // if table is created, this code will just be ignored
+            connection.Insert(contact); // Insert method will be able to find which table to insert the new record
+            connection.Close();
+
             this.Close();
         }
     }
