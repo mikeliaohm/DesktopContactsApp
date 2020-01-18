aecd14bc052452c82767b69e58005f5dfb83c6b0
Mike Liao <mike.hm.liao@gmail.com>
2020-01-18 10:48:21 +0800

    added new contact window

--- DesktopContactsApp/NewContactWindow.xaml.cs
+++ DesktopContactsApp/NewContactWindow.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DesktopContactsApp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for NewContactWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class NewContactWindow : Window
     {
-        public MainWindow()
+        public NewContactWindow()
         {
             InitializeComponent();
-        }
-
-        private void saveButton_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
